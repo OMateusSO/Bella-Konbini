316d37ed181980fbc907c1eaaa3eb0fc3ed631c8
Dali <82356573+OMateusSO@users.noreply.github.com>
2025-09-25 13:23:30 -0300

    4th commit

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <div class="min-h-screen flex flex-col">
     <Nav />
     <main class="flex-1 pt-12">
@@ -11,15 +10,3 @@
   </div>
 </template>
 
-=======
-  <div class="">
-    <Nav/>
-    <main class="py-12"> 
-      <NuxtLayout>
-        <NuxtPage/>
-      </NuxtLayout>
-    </main>
-
-  </div>
-</template>
->>>>>>> ba3ebe00ba85a6f6548969c15612899cd845964b
